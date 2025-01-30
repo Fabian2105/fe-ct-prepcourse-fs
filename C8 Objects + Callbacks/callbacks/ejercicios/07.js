7dc52427d344c9e3e89c93454055ed835987aa7d
Fabian21 <yonderjef@gmail.com>
2025-01-30 17:07:21 -0400

    Actualizacion nueva

--- C8 Objects + Callbacks/callbacks/ejercicios/07.js
+++ C8 Objects + Callbacks/callbacks/ejercicios/07.js
@@ -2,6 +2,7 @@ function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
    // Tu código:
+   return arrayOfStrings.filter(str => str.toLowerCase().startsWith("a"));
 }
 
 module.exports = filter;
